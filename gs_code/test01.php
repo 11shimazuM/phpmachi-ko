edbc41225d28988ff0b63dd152f361c3b6c68867
11shimazuM <95361235+11shimazuM@users.noreply.github.com>
2022-01-22 00:57:42 +0900

    Add files via upload

---
+++ gs_code/test01.php
@@ -0,0 +1,18 @@
+<!-- PHP-01-02 -->
+<!-- <?php
+echo "あああああ<br>";
+echo "いいいいい<br>";
+echo "ううううう<br>";
+?> -->
+
+<!-- PHP-01-03 -->
+<?php
+$name = "あいうえお<br>";
+$num  =  100;
+$a = 1;
+$b = 2;
+$c = 3;
+
+echo $name;
+echo $num;
+echo $a + $b + $c;
